70b75828e03231055f687f5c9d4932c066221ffb
Devano Brown <Devano.Brown@smu.ca>
2019-05-15 13:31:06 -0300

    Small Update-Added textExtramction

---
+++ textExtraction.py
@@ -0,0 +1,26 @@
+
+class textExtraction:
+    
+    text = ""
+
+    def setText(self, text):
+        self.text = text
+    def removeNonAscii(self):
+        tempString = ""c 
+        for char in self.text:
+            if ()
+
+    def processText(self):
+        if not self.text: ## if the text is empty then do nothing
+            return self.text
+        ##remove trailing spaces from both end of the string 
+        self.text = self.text.strip()
+        ##remove any special or non ascii characters
+        
+
+    def getProcessedText(self):
+        pass
+    
+    def getRawText(self):
+
+
