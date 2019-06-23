436a596af9138bb8a61cc3fd139a90a7f8946cdf
Devano Brown <browndevano@gmail.com>
2019-06-23 12:56:10 -0300

    DataBase Testing

--- SQL/dataBase.py
+++ SQL/dataBase.py
@@ -1,11 +1,33 @@
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy import MetaData
+from databases import Database
+import asyncio
 
 class DataBaseComponet:
-    pass
+
+    def __init__(self):
+        #create database connection 
+         self.database = Database('sqlite:///BayesianDataBase.db')
+         #get event loop
+         self.eventLoop = asyncio.get_event_loop()
+     
+    async def setUpDataBase(self):
+        pass
+
+
+async def main():
+    database = Database('sqlite:///BayesianDataBase.db')
+    await database.connect()
+    query = "CREATE TABLE IF NOT EXISTS ClassificationType \
+    (ID  INTEGER PRIMARY KEY, Classification VARCHAR(200) )"
+    result = await database.execute(query=query)
+    insert = "INSERT INTO ClassificationType (Classification) VALUES ('Hellohvhvhvhvhvhvhvtycycycycycg')"
+    result = await database.execute(query=insert)
+    getRows= "Select * from ClassificationType"
+    result= await database.fetch_all(query=getRows)
+    print(result)
+
 
 if __name__ == "__main__":
-    metadata = MetaData()
-    engine = create_engine('sqlite:///bookstore.db')
-    metadata.create_all(engine)
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
+   
